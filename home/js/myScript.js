57d9410f61caccf867068ad506475b4982fc5003
Charijoy Cempron <134136959+cha-ryne@users.noreply.github.com>
2024-12-16 16:32:32 +0800

    try

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,9 +1,11 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
 
-  function myFunction2() {
-    const element = document.getElementsByTagName("p");
+function myFunction2() {
+  const element = document.getElementsByTagName("p");
 
-    document.getElementById("demo").innerHTML = '<br> The text in first paragraph (index 0) is: <br>' + element[0].innerHTML;
-  }
+  document.getElementById("demo").innerHTML = '<br> The text in first paragraph (index 0) is: <br>' + element[0].innerHTML;
+  
+
+}
