48873bbe0ab978140bb0cb3e837822ce2b049319
Charijoy Cempron <134136959+cha-ryne@users.noreply.github.com>
2025-02-01 05:54:21 +0000

    supabase comments

---
+++ my-app/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://xhsptturvttitkhfyvhh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhoc3B0dHVydnR0aXRraGZ5dmhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5NzA3MTIsImV4cCI6MjA1MzU0NjcxMn0.8iJz3JyEYOzAswXfoAWwXu2T5Rver2H4xRM1-e5wzKU')
