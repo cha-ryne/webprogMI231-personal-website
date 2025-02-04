d7d3c5c7df9b9494242ee70bffcac1ad71556eb3
cha-ryne <134136959+cha-ryne@users.noreply.github.com>
2025-02-05 02:41:39 +0800

    try

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import SurveyComponent from "./components/SurveyComponent";
 
-createApp(App).mount('#app')
+new Vue({
+    el: "#app",
+    template: '<div id="surveyElement" style="position: absolute; top: 0; left: 0; right: 0; bottom: 0; min-height: 100%; height:100%"> <survey-component /></div>',
+    component: {
+        "survey-component": SurveyComponent
+    }
+});
