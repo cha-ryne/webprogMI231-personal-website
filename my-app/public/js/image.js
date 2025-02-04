4c1ea1d4517a026d0a44a406b5b949cd1e36ad0d
cha-ryne <134136959+cha-ryne@users.noreply.github.com>
2025-02-05 01:39:41 +0800

    trrrrrrrry

--- my-app/public/js/image.js
+++ my-app/public/js/image.js
@@ -14,12 +14,12 @@ new Vue({
     },
     data: {
       images: [
-        'images/Sakamoto.jpg',
-        'images/bp.jpg',
-        'images/Manga.jpg',
-        'images/pirates.jpg',
+        '/images/Sakamoto.jpg',
+        '/images/bp.jpg',
+        '/images/Manga.jpg',
+        '/images/pirates.jpg',
         'https://preview.redd.it/new-wallpaper-for-my-pc-they-have-no-right-being-this-cool-v0-3l2k9lpytrcc1.jpeg?auto=webp&s=84fbc9925af40466495e023248afa37305b232fd',
-        'images/Chman.jpg',
+        '/images/Chman.jpg',
       ],
       index: 0,
       showSlideshow: false
