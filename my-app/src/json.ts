00f2de398093362e29baa7cf3322cc06286d3971
Charijoy Cempron <134136959+cha-ryne@users.noreply.github.com>
2025-02-05 18:31:53 +0000

    try

---
+++ my-app/src/json.ts
@@ -0,0 +1,21 @@
+export const json = {
+    "pages": [
+      {
+        "name": "page1",
+        "elements": [
+          {
+            "type": "rating",
+            "name": "satisfaction-smileys-colored",
+            "title": "How satisfied are you with our product?",
+            "description": "Smiley rating with colored scale",
+            "rateType": "smileys",
+            "scaleColorMode": "colored",
+            "rateCount": 10,
+            "rateMax": 10,
+            "displayMode": "buttons"
+          }
+        ]
+      }
+    ],
+    "showQuestionNumbers": "off"
+  }
